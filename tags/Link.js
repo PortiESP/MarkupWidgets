012ccdaef2f10924b218238be95a83d4572e53b1
Porti <elp0rti201@gmail.com>
2022-07-19 10:50:38 +0200

    Link and image bugs fixed

--- tags/Link.js
+++ tags/Link.js
@@ -3,5 +3,5 @@ import {link as className} from "./styles.module.scss"
 
 export default function Link(props){
 
-    return <a className={className} href={props.href || "#"} style={props.style}>{props.children || props.text}</a>
+    return <a className={className} href={props.href || "#"} style={props.style} id={props.id} title={props.title}>{props.children || props.text}</a>
 }
