f907ed040e036c107151bbb7f4d6e5cdefd42528
Porti <elp0rti201@gmail.com>
2022-07-22 13:07:39 +0200

    Added title4, minor style changes

--- Tags.js
+++ Tags.js
@@ -1,6 +1,7 @@
 import Title from "./tags/Title"
 import Title2 from "./tags/Title2"
 import Title3 from "./tags/Title3"
+import Title4 from "./tags/Title4"
 import Paragraph from "./tags/Paragraph"
 import Code from "./tags/Code"
 import Img from "./tags/Img"
@@ -11,8 +12,8 @@ import Link from "./tags/Link"
 import Hr from "./tags/Hr"
 import Br from "./tags/Br"
 
-const tags = { Title, Title2, Title3, Paragraph, Code, Img, UList, Callout, Quote, Link, Hr, Br }
+const tags = { Title, Title2, Title3, Title4, Paragraph, Code, Img, UList, Callout, Quote, Link, Hr, Br }
 
 // Exports
 export default tags; // Export all
-export { Title, Title2, Title3, Paragraph, Code, Img, UList, Callout, Quote, Link, Hr, Br }; // Export individual
+export { Title, Title2, Title3, Title4, Paragraph, Code, Img, UList, Callout, Quote, Link, Hr, Br }; // Export individual
---
+++ tags/Title4.js
@@ -0,0 +1,9 @@
+import {title4 as className} from "./styles.module.scss"
+
+export default function Title4(props){
+    return (
+        <>
+            <h4 className={className} style={props.style} id={props.id}>{props.children || props.text}</h4>
+        </>
+    )
+}
