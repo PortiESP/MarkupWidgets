ef828f49e3e66418e45547b689953830d0f78b65
Porti <elp0rti201@gmail.com>
2022-07-25 17:21:07 +0200

    Carousel animated

--- tags/Carousel.js
+++ tags/Carousel.js
@@ -3,10 +3,10 @@ import sass, {carousel as className} from "./styles/carousel.module.scss"
 
 export default function Carousel(props){
 
-    const [currentCard, setCurrentCard] = useState([4,1])
+    const [currentCard, setCurrentCard] = useState(4)
 
     const getPos = center => [ (center) ? center-1: props.cards.length-1, (center+1) % props.cards.length ];
-    const [prevCard, nextCard] = getPos(currentCard[0]) 
+    const [leftCard, rightCard] = getPos(currentCard) 
 
  
 
@@ -18,24 +18,23 @@ export default function Carousel(props){
                  {props.cards.map( (card, i) => {
                      
                      const extraClass =  
-                     i===prevCard && sass.prev || 
-                     i===nextCard && sass.next || 
-                     i===currentCard[0] && sass.currentCard ||
-                     i===getPos(nextCard)[1] && sass.hiddenNext || 
-                     i===getPos(prevCard)[0] && sass.hiddenPrev || 
+                     i===leftCard && sass.left || 
+                     i===rightCard && sass.right || 
+                     i===currentCard && sass.currentCard ||
+
                      ""
                      
-                     return <div key={i} className={[sass.div__card, extraClass,  currentCard[1]===-1 && sass.reverse].join(" ")}>{props.cards[i]}</div>
+                     return <div key={i} className={[sass.div__card, extraClass].join(" ")}>{props.cards[i]}</div>
                      
-                    })}
+                })}
                 
             </div>
             <div className={sass.div__bubbles}>
-                {props.cards.map( (_, i) => <div key={i} className={[sass.bubble, i===currentCard[0] && sass.currentBubble].join(" ")} onClick={()=>setCurrentCard([i,0])}></div>)}
+                {props.cards.map( (_, i) => <div key={i} className={[sass.bubble, i===currentCard && sass.currentBubble].join(" ")} onClick={()=>setCurrentCard(i)}></div>)}
             </div>
             <div className={sass.div__buttons}>
-                <button onClick={()=>{setCurrentCard([prevCard, -1])}}>{"<"}</button>
-                <button onClick={()=>{setCurrentCard([nextCard, 1])}}>{">"}</button>
+                <button onClick={()=>{setCurrentCard(leftCard)}}>{"<"}</button>
+                <button onClick={()=>{setCurrentCard(rightCard)}}>{">"}</button>
             </div>
         </div>
         </>
