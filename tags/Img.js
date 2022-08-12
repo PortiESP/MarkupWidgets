40119bcb12544cc681b8753aff4221fb0317faab
Porti <elp0rti201@gmail.com>
2022-08-12 15:22:10 +0200

    updated img

--- tags/Img.js
+++ tags/Img.js
@@ -1,12 +1,13 @@
-import sass, {image as className} from "./styles/image.module.scss"
+import sass from "./styles/image.module.scss"
 import Image from "next/image"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 
 export default function Img(props){
 
     const [zoom, setZoom] = useState(false)
     const [img, setImg] = useState(null)
-    const [caption, setCaption] = useState(null)
+    const router = useRouter()
 
     useEffect(()=>{
         setImg( 
@@ -19,29 +20,22 @@ export default function Img(props){
                 <Image src={props.img} alt={props.alt} {...props.img} objectFit="contain"/>
         )
 
-        setCaption(
-            props.caption ?
-                <div className={sass.caption}>
-                    <span>{props.caption}</span>
-                </div>:
-                ""
-        )
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
         <>
-            <div className={className} style={props.style} id={props.id} title={props.title} onClick={()=>setZoom(true)}>
+            <div className={sass.image} style={{ height: props.height, ...props.style}} id={props.id} title={props.title} onClick={()=>setZoom(true)}>
                 {img}
             </div>
             {
                 zoom &&
                 <div className={sass.div__zoom_wrap} onClick={ e => e.target.className === sass.div__zoom_wrap && setZoom(false)}>
-                    <div className={sass.div__zoomImage}>
-                        {img}
+                    <div className={sass.div__zoomImage} onClick={ () => props.href && router.push(props.href)}>
+                        <Image src={props.img} alt={props.alt} layout="fill" objectFit="contain"/>
                     </div>
-                    { props.hideCaption || <span><i>{props.title || props.alt}</i></span> }
+                    { props.hideCaption || !props.caption || <span><i>{props.title || props.alt}</i></span> }
                     
                 </div>
             }
