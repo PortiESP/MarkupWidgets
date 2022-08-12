3e28f97a169ac86b2e525a73193ceb13c1135880
Porti <elp0rti201@gmail.com>
2022-08-12 22:18:34 +0200

    up bullet color

--- tags/UList.js
+++ tags/UList.js
@@ -3,6 +3,11 @@ import {ulist as className} from "./styles/ul.module.scss"
 export default function UList(props){
     return (
         <>
+            <style jsx="true">{`
+                .${className}{
+                    ${props.bulletColor && `--bulletColor:${props.bulletColor};`}
+                }
+            `}</style>
             <ul className={className} style={props.style} id={props.id}>
                 {props.items.map( (item, i) => <li key={i} style={props.itemStyle}>{item}</li>)}
             </ul>
