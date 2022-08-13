b8556522e17eef7754316b8d839e0c03f43b74cb
Porti <elp0rti201@gmail.com>
2022-08-13 14:14:04 +0200

    Link target blank

--- tags/Link.js
+++ tags/Link.js
@@ -3,5 +3,5 @@ import {link as className} from "./styles/link.module.scss"
 
 export default function Link(props){
 
-    return <a className={className} href={props.href || "#"} style={props.style} id={props.id} title={props.title}>{props.children || props.text}</a>
+    return <a className={className} target="_blank" rel="noreferrer" href={props.href || "#"} style={props.style} id={props.id} title={props.title}>{props.children || props.text}</a>
 }
