fa23dba4b57da1664a9490b09acfae638a85a668
Porti <elp0rti201@gmail.com>
2022-08-17 00:18:55 +0200

    url props id and style

--- tags/Url.js
+++ tags/Url.js
@@ -4,7 +4,7 @@ import sass from "./styles/url.module.scss"
 export default function Url(props){
 
     return (
-        <div className={sass.div__url_wrap} onClick={ ()=> window.location.href = props.src}>
+        <div className={sass.div__url_wrap} onClick={ ()=> window.location.href = props.src} style={props.style} id={props.id}>
             <div className={sass.div__url_data}>
                 <div className={sass.div__data_title}>{props.title}</div>
                 <div className={sass.div__data_description}>{props.description}</div>
