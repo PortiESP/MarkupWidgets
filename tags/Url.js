66952b71cc1165dba16b4ba03e9c251f828ba194
Porti <elp0rti201@gmail.com>
2022-08-27 13:48:33 +0200

    Update url and section responsive

--- tags/Url.js
+++ tags/Url.js
@@ -1,12 +1,18 @@
-import { useEffect } from "react"
 import sass from "./styles/url.module.scss"
+import icon from "../assets/icons/enlace.png"
+import Image from "next/image"
 
 export default function Url(props){
 
     return (
         <div className={sass.div__url_wrap} onClick={ ()=> window.location.href = props.src} style={props.style} id={props.id}>
             <div className={sass.div__url_data}>
-                <div className={sass.div__data_title}>{props.title}</div>
+                <div className={sass.div__data_title}>
+                    <span>{props.title}</span>
+                    <div className={sass.div__title_img}>
+                        <Image src={icon} alt="link icon" layout="fill" objectFit="contain"/>
+                    </div>
+                </div>
                 <div className={sass.div__data_description}>{props.description}</div>
                 <div className={sass.div__data_src}>{props.src}</div>
             </div>
