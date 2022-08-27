71621279c85b70b8ae40997c5b8cfc75d0155c5d
Porti <elp0rti201@gmail.com>
2022-08-27 16:59:40 +0200

    Url copy to clipboard

--- tags/Url.js
+++ tags/Url.js
@@ -1,28 +1,34 @@
 import sass from "./styles/url.module.scss"
 import icon from "../assets/icons/enlace.png"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Url(props){
 
     return (
-        <div className={sass.div__url_wrap} onClick={ ()=> window.location.href = props.src} style={props.style} id={props.id}>
-            <div className={sass.div__url_data}>
-                <div className={sass.div__data_title}>
-                    <span>{props.title}</span>
-                    <div className={sass.div__title_img}>
-                        <Image src={icon} alt="link icon" layout="fill" objectFit="contain"/>
+        <Link href={props.src} target="_blank" passHref>
+            <div className={sass.div__url_wrap} style={props.style} id={props.id}>
+                <div className={sass.div__url_data}>
+                    <div className={sass.div__data_title}>
+                        <span>{props.title}</span>
+                        <div className={sass.div__title_img} onClick={ e => {
+                                    e.preventDefault()
+                                    navigator.clipboard.writeText(props.src)
+                                }}>
+                            <Image src={icon} alt="link icon" layout="fill" objectFit="contain" title="Copy url" />
+                        </div>
                     </div>
+                    <div className={sass.div__data_description}>{props.description}</div>
+                    <div className={sass.div__data_src}>{props.src}</div>
                 </div>
-                <div className={sass.div__data_description}>{props.description}</div>
-                <div className={sass.div__data_src}>{props.src}</div>
+                {
+                    props.img &&
+                    <div className={sass.div__url_img}>
+                        <Image src={props.img} alt="Favicon" layout="fill" objectFit="cover"/>
+                        <Image src={props.img} alt="Favicon" layout="fill" objectFit="contain"/>
+                    </div>
+                }
             </div>
-            {
-                props.img &&
-                <div className={sass.div__url_img}>
-                    <Image src={props.img} alt="Favicon" layout="fill" objectFit="cover"/>
-                    <Image src={props.img} alt="Favicon" layout="fill" objectFit="contain"/>
-                </div>
-            }
-        </div>
+        </Link>
     )
 }
