658112a6cd5f749d5a48a2bb4c6115913127d2dc
Porti <you@example.com>
2023-03-11 12:11:03 +0100

    Quote style updated

--- tags/Quote.js
+++ tags/Quote.js
@@ -6,7 +6,7 @@ export default function Quote(props){
     return (
         <>
             <div className={className} style={{...props.style, "--color": props.color}} id={props.id}>
-                <h4>{props.title}</h4>
+                {props.title && <h4>{props.title}</h4>}
                 <div>{props.children || props.text}</div>
             </div>
         </>
